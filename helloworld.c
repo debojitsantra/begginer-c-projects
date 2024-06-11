db5979c0f0044e8c386ab1917cec5ffc806bef83
Debojit Santra <debojitsantra48@gmail.com>
2024-06-11 22:43:44 +0530

    update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,4 @@
 #include <stdio.h> //Basic Input Output file 
 void main() { //here void means no other value is returning
-  printf("Hello World");
+  printf("Hello World"); //basic print function..used to print valus
 }
